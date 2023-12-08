ace6433ffc975defdca38304a9a83fb05ad05ff2
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-12-08 06:02:41 +0300

    refactor(frontend): rework cookie utility file

--- react-ui/src/utils/cookies.js
+++ react-ui/src/utils/cookies.js
@@ -6,7 +6,7 @@
  * @return {string|null} The value of the cookie if it exists.
  * Otherwise, returns null.
  */
-export const getCookie = (name) => {
+const getCookie = (name) => {
     let end;
     const dc = document.cookie;
     const prefix = name + '=';
@@ -24,3 +24,11 @@ export const getCookie = (name) => {
 
     return decodeURI(dc.substring(begin + prefix.length, end));
 };
+
+const getCookieValue = (name) => {
+    const cookies = document.cookie.split('; ');
+    const cookie = cookies.find((c) => c.startsWith(`${name}=`));
+    return cookie ? cookie.split('=')[1] : null;
+};
+
+export {getCookie, getCookieValue};
