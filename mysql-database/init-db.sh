80961e6e4269ec3d27e449f378f73bfbb31fab60
Vladislav Nasevich <91089134+watisluv1910@users.noreply.github.com>
2024-05-20 15:44:29 +0300

    refactor: update database initialization scripts

--- mysql-database/init-db.sh
+++ mysql-database/init-db.sh
@@ -5,7 +5,7 @@ set -e
 # Set the current directory to the script's directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-tr -d '\r' < .env > .env.new && mv .env.new .env
+tr -d '\r' < ../.env.example > .env.tmp && mv .env.tmp ../.env
 
 source ../.env
 
@@ -13,7 +13,9 @@ TMP_SCRIPT=$(mktemp /tmp/temp_schema.XXXXXX)
 trap 'rm -f $TMP_SCRIPT' EXIT
 
 while IFS= read -r line; do
-  echo "${line//\${DB_SCHEMA}/$MYSQL_DATABASE}" >> "$TMP_SCRIPT"
+  echo "${line//\$DB_SCHEMA/$MYSQL_DATABASE}" >> "$TMP_SCRIPT"
 done < schema.sql
 
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" < "$TMP_SCRIPT"
+# Using --defaults-extra-file with printf to prevent
+# 'Using a password on the command line interface can be insecure' warning
+mysql --defaults-extra-file=<(printf "[client]\nuser = %s\npassword = %s" "$MYSQL_USER" "$MYSQL_PASSWORD") < "$TMP_SCRIPT"
