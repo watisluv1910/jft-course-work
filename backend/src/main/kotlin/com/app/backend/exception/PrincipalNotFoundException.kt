e47c160432b6245f8bfe3588595745f0feb39dc9
Vladislav Nasevich <91089134+watisluv1910@users.noreply.github.com>
2024-05-21 15:03:40 +0300

    fix: make PrincipalNotFoundException kotlin class (was Java class)

--- backend/src/main/kotlin/com/app/backend/exception/PrincipalNotFoundException.kt
+++ backend/src/main/kotlin/com/app/backend/exception/PrincipalNotFoundException.kt
@@ -1,30 +1,26 @@
-package com.app.backend.exception;
+package com.app.backend.exception
 
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.AuthenticationException
 
 /**
  * @author Vladislav Nasevich
  */
-public class PrincipalNotFoundException extends AuthenticationException {
+class PrincipalNotFoundException : AuthenticationException {
 
     /**
-     * Constructs an {@code AuthenticationException} with the specified message and no
+     * Constructs an `AuthenticationException` with the specified message and no
      * root cause.
      *
-     * @param msg the detail message
+     * @param msg the detail message.
      */
-    public PrincipalNotFoundException(String msg) {
-        super(msg);
-    }
+    constructor(msg: String?) : super(msg)
 
     /**
-     * Constructs an {@code AuthenticationException} with the specified message and root
+     * Constructs an `AuthenticationException` with the specified message and root
      * cause.
      *
-     * @param msg   the detail message
-     * @param cause the root cause
+     * @param msg   the detail message.
+     * @param cause the root cause.
      */
-    public PrincipalNotFoundException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
+    constructor(msg: String?, cause: Throwable?) : super(msg, cause)
 }
